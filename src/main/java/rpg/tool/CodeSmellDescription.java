4f19314e0954ce067e192d8e91b7a394cf7f5656
Patrick <berox@rocketmail.com>
2017-10-24 22:58:03 +0200

    Simple example of a case which might go unnoticed by an inexperienced coder. A simple rename action might actually result in changed behavior of the code, when the side effects are not clear.

---
+++ src/main/java/rpg/tool/CodeSmellDescription.java
@@ -0,0 +1,18 @@
+package rpg.tool;
+
+/**
+ * Data structure containing information about a code smell that has been detected by
+ * a third party tool
+ */
+public class CodeSmellDescription {
+
+    // Name of the smell (used to find the corresponding template with procedural guidance info
+    public String Name = "";
+
+    // Name of java file where smell was detected
+    public String JavaFile = "";
+
+    // Location (line number) in the file where the smell is detected
+    public int fileLocation = 0;
+    
+}
