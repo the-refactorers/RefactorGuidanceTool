02068fa61b471a0c80d5df59d7d4e3d78d3a37de
Patrick de Beer <p.debeer@fontys.nl>
2018-04-08 22:34:35 +0200

    instanceof instead of using Javaparser metadata

--- src/main/java/analysis/dataflow/LocalVariableReadMarker.java
+++ src/main/java/analysis/dataflow/LocalVariableReadMarker.java
@@ -7,6 +7,12 @@ import com.github.javaparser.ast.expr.*;
 
 import java.util.Optional;
 
+/**
+ *
+ * Responsibility of this class is to mark those variables in the provided VariableFlowSet
+ * which are read in the code.
+ *
+ */
 public class LocalVariableReadMarker extends MarkVariableFlowList {
 
     public LocalVariableReadMarker(MethodDeclaration md) {
@@ -25,31 +31,36 @@ public class LocalVariableReadMarker extends MarkVariableFlowList {
         {
             Optional<Node> parentNode = sn.getParentNode();
 
-            String name = parentNode.get().getMetaModel().getTypeName();
+            if (parentNode.isPresent()) {
 
-            // todo: Code below can  be generalized. e.g. set of strategies. Or a chain of functionality, where new handling can be added
-            // visitor pattern can also be a solution
-            if(name.contains("AssignExpr")) {
-                AssignExpr ae = (AssignExpr)parentNode.get();
+                try {
+                    // todo: Code below can  be generalized. e.g. set of strategies. Or a chain of functionality, where new handling can be added
+                    // visitor pattern can also be a solution
+                    if (parentNode.get() instanceof AssignExpr) {
+                        AssignExpr ae = (AssignExpr) parentNode.get();
 
-                // when name of target variable is not the name of the variable in the FlowTable
-                // the conclusion we are dealing with the variables that are read in this expression
-                if (!ae.getTarget().toString().contains(flowTable.name))
-                    MarkFlowTable(flowTable, E_ACTION.read, startLine(ae.getRange()));
-            }
-            else
-            if(name.contains("MethodCallExpr")) {
-                MethodCallExpr mce = (MethodCallExpr)parentNode.get();
+                        // when name of target variable is not the name of the variable in the FlowTable
+                        // the conclusion we are dealing with the variables that are read in this expression
+                        if (!ae.getTarget().toString().contains(flowTable.name))
+                            MarkFlowTable(flowTable, E_ACTION.read, startLine(ae.getRange()));
+                    } else if (parentNode.get() instanceof MethodCallExpr) {
+                        MethodCallExpr mce = (MethodCallExpr) parentNode.get();
 
-                NodeList<Expression> allMethodArguments = mce.getArguments();
+                        NodeList<Expression> allMethodArguments = mce.getArguments();
 
-                // Arguments passed as variables which are present in one of the local declared
-                // flowtables, can be concluded that this is a variable where information is read from
-                allMethodArguments.forEach(argument ->
+                        // Arguments passed as variables which are present in one of the local declared
+                        // flowtables, can be concluded that this is a variable where information is read from
+                        allMethodArguments.forEach(argument ->
+                        {
+                            if (argument.toString().contains(flowTable.name))
+                                MarkFlowTable(flowTable, E_ACTION.read, startLine(mce.getRange()));
+                        });
+                    }
+                }
+                catch(ClassCastException ce)
                 {
-                    if(argument.toString().contains(flowTable.name))
-                        MarkFlowTable(flowTable, E_ACTION.read, startLine(mce.getRange()));
-                });
+                    System.out.println(ce.getMessage());
+                }
             }
         });
     }
